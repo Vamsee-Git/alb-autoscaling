12004e4ebeb24df640b01cf41007b2c34a750616
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 18:08:41 +0530

    Update main.tf

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -1,4 +1,9 @@
 # modules/vpc/main.tf
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
