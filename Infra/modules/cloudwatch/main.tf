64917faa8408a75219d48fd17947934335cdb9eb
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 17:43:22 +0530

    Create main.tf

---
+++ Infra/modules/cloudwatch/main.tf
@@ -0,0 +1,25 @@
+resource "aws_sns_topic" "alerts" {
+  name = "cpu-alerts-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_alert" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "your-email@example.com"  # Replace with your email
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "HighCPUUtilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors high CPU usage"
+  dimensions = {
+    AutoScalingGroupName = var.asg_name
+  }
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
