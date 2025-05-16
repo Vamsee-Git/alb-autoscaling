a9e9dbadeb19cf52415015395a6951b318093442
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 07:32:52 +0530

    Update main.tf

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -35,10 +35,46 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 
 output "public_subnets" {
   value = aws_subnet.public[*].id
+}
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
 }
