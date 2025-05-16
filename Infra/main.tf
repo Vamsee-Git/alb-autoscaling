8a7ea9a7c2ff100dfea3f47a24ff90085fe5efd5
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 07:41:27 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -19,6 +19,7 @@ module "ec2" {
   source = "./modules/ec2"
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
+  security_group_id = module.vpc.security_group_id
 }
 
 module "alb" {
